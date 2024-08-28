4944f803eb33ba1a27b5a3dea0299070516cd6c3
toshioishibashi <145513956+toshioishibashi@users.noreply.github.com>
2024-08-27 22:11:24 -0300

    Add files via upload

---
+++ hello-world/src/main/java/br/com/julio/HelloWorld.java
@@ -0,0 +1,21 @@
+package br.com.julio;
+
+public class HelloWorld {
+
+	private final long id;
+	private final String content;
+	
+	public HelloWorld(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}	
+	
+}
